aac60793758a1927b1a188be6b5270102e3b89a3
slipova <sofiya.lipova@mail.mcgill.ca>
2022-03-09 14:05:47 -0500

    create guessing game

---
+++ guessing_game/guessing_game.py
@@ -0,0 +1,17 @@
+import random
+
+def guess_number(x):
+  random_number = random.randint(1, x)
+  guess = 0
+
+  while guess != random_number:
+    guess = int(input(f"Pick a number between 1 and {x}"))
+    if guess > random_number:
+      print("Your number is too high! Guess again.")
+    elif guess < random_number:
+      print("Your number is too low! Guess again.")
+
+  print("You got it!")
+
+
+guess_number(10)
