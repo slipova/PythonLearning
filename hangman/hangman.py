12d6ff27049939a24421b94f8755342d3d97a861
slipova <sofiya.lipova@mail.mcgill.ca>
2022-03-14 22:49:37 -0400

    define function for random word

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,3 +1,14 @@
+import random
 from words import words
 
-print(words)
+def get_valid_word(words):
+  word = random.choice(words)
+
+  while '-' in word or ' ' in word:
+    word = random.choice(words)
+
+  print(word)
+  return word
+
+
+get_valid_word(words)
